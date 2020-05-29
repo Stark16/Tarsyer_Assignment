fa1b39cbba535a412813f4be0143b470d73d727e
Stark16 <pradyumnp508@gmail.com>
2020-05-29 15:15:24 +0530

    Started 2nd Assignment script

--- Assignment 2/Re-arrange.py
+++ Assignment 2/Re-arrange.py
@@ -1,25 +1,55 @@
 import os
 from xml.etree import ElementTree
 import random
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
 
-def arrange_char(filename, Char_data):
+def plot_diff(Char_data):
+    difference = []
+    x = []
+    y = []
+    for i in range(len(Char_data)):
+        diff = abs(Char_data[0][1][1] - Char_data[i][1][1])
+        difference.append([Char_data[i][0], diff])
+        x.append(Char_data[i][0])
+        y.append(diff)
+    print(difference)
+    plt.scatter(x, y)
+    plt.show()
+
+
+def arrange_nums(nums, final_sequence):
+
+    # Sorting the sequence of numbers based on y values in ascending order:
+    nums.sort(key = lambda x: x[1][1], reverse=False)
+
+    if nums[0][1][0] < nums[1][1][0]:
+        final_sequence[2] = nums[0]
+        final_sequence[3] = nums[1]
+    else:
+        final_sequence[2] = nums[1]
+        final_sequence[3] = nums[0]
+    #print(final_sequence)
+
+
+
+def Arrange(filename, Char_data, final_sequence, width, height):
     chars = []
     nums = []
     numberplate = []
+
     for i in range(len(Char_data)):
-       if Char_data[i][0].isnumeric() == False:
-           chars.append(Char_data[i])
-       else:
-           nums.append(Char_data[i])
 
-    for i in range(len(chars)):
-        # alph, centre = chars[i]
+        if Char_data[i][0].isnumeric() == False:
+            chars.append(Char_data[i])
+        else:
+            nums.append(Char_data[i])
 
-        if chars[i][1][1] < chars[i+1][1][1]:
-            if abs([i][1][0] - chars[i+1][1][0]) < abs([i][1][1] - chars[i+1][1][1]):
-                numberplate.append(chars[i])
+    #arrange_char(chars, final_sequence)
 
-
+    arrange_nums(nums, final_sequence)
+    
     # print(chars)
 
 
@@ -28,7 +58,13 @@ def arrange_char(filename, Char_data):
 def read_from_xml(filepath, filename):
     xml_obj = ElementTree.parse(os.path.join(filepath, filename))
     objects = xml_obj.findall('object')
+
+    dim_obj = xml_obj.find('size')
+    width = int(dim_obj.find('width').text)
+    height = int( dim_obj.find('height').text)
+
     Char_data = []
+    final_sequence = [None] * 10
     for ob in objects:
         char = ob.find('name').text
 
@@ -53,7 +89,8 @@ def read_from_xml(filepath, filename):
             #   )
     random.shuffle(Char_data)
     #print(Char_data)
-    arrange_char(filename, Char_data)
+    plot_diff(Char_data)
+    Arrange(filename, Char_data, final_sequence, width, height)
 
 
 
@@ -63,3 +100,8 @@ def read_from_xml(filepath, filename):
 for file in os.listdir("./Char-detection"):
     if file[-3:] == 'xml':
         read_from_xml("./Char-detection", file)
+
+
+'''  img = np.zeros((height, width, 3), dtype=np.uint8) 
+    cv2.imshow("img", img)
+    #cv2.waitKey(0)'''
