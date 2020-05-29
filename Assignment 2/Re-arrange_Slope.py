e8d4b27bb8be0b15922679d4665bb51dfda50b35
Stark16 <pradyumnp508@gmail.com>
2020-05-29 23:54:42 +0530

    Added 2nd script to find the shape of number plate using slop of curve

--- Assignment 2/Re-arrange_Slope.py
+++ Assignment 2/Re-arrange_Slope.py
@@ -6,36 +6,57 @@ import cv2
 import numpy as np
 import math
 
-def plot_diff(Char_data):
-    difference = []
+
+def check_shape(Char_data, width, height):
+    jumps = 0
     x = []
     y = []
-    mean = 0
+
     for i in range(len(Char_data)):
-        diff = abs(Char_data[0][1][1] - Char_data[i][1][1])
-        if i != 0:
-            difference.append([Char_data[i][0], diff])
-            x.append(i)
-            y.append(diff)
-        mean += diff
-    print(difference)
-    mean = mean/len(Char_data)
-    nm = 0
-    for i in range(len(difference)):
-        nm += (int(difference[i][1]) - mean) ** 2
-    sd = math.sqrt(nm/len(difference))
-    varience = math.sqrt(nm/(len(difference)-1))
-    print("Mean: {}, SD: {}, varience{}".format(mean, sd, varience))
-    #plt.plot(x, y)
-    #ax = plt.gca()
-    #ax.set_ylim([0, 100])
-    #plt.show()
+        x.append(Char_data[i][1][0])
+        y.append(Char_data[i][1][1])
+
+    x.sort(reverse=True)
+    y.sort(reverse=True)
+
+    for i in range(len(x)):
+        try:
+            x1 = (x[i])
+            y1 = (y[i])
+            x2 = (x[i+1])
+            y2 = (y[i+1])
+            if abs(x2 - x1) <= abs(y2 - y1):
+                jumps += 1
+        except:
+            continue
+    print(x, y)
+    print(jumps)
+
+    '''for i in range(len(Char_data)):
+        try:
+            x1 = (Char_data[i][1][0])
+            x.append(x1)
+            y1 = (Char_data[i][1][1])
+            y.append(y1)
+            x2 = (Char_data[i+1][1][0])
+            y2 = (Char_data[i+1][1][1])
+            if abs(x2 - x1) <= abs(y2 - y1):
+                    jumps += 1
+        except:
+            continue'''
+    plt.plot(x, y)
+    ax = plt.gca()
+    ax.set_ylim([0, height])
+    ax.set_xlim([0, width])
+    plt.show()
+
+    return 1 if jumps >= 1 else 0
+
 
 
 def arrange_nums(nums, final_sequence):
-
     # Sorting the sequence of numbers based on y values in ascending order:
-    nums.sort(key = lambda x: x[1][1], reverse=False)
+    nums.sort(key=lambda x: x[1][1], reverse=False)
 
     if nums[0][1][0] < nums[1][1][0]:
         final_sequence[2] = nums[0]
@@ -43,8 +64,7 @@ def arrange_nums(nums, final_sequence):
     else:
         final_sequence[2] = nums[1]
         final_sequence[3] = nums[0]
-    #print(final_sequence)
-
+    # print(final_sequence)
 
 
 def Arrange(filename, Char_data, final_sequence, width, height):
@@ -59,13 +79,11 @@ def Arrange(filename, Char_data, final_sequence, width, height):
         else:
             nums.append(Char_data[i])
 
-    #arrange_char(chars, final_sequence)
+    # arrange_char(chars, final_sequence)
 
     arrange_nums(nums, final_sequence)
-    
+
     # print(chars)
-
-
 
 
 def read_from_xml(filepath, filename):
@@ -74,13 +92,12 @@ def read_from_xml(filepath, filename):
 
     dim_obj = xml_obj.find('size')
     width = int(dim_obj.find('width').text)
-    height = int( dim_obj.find('height').text)
+    height = int(dim_obj.find('height').text)
 
     Char_data = []
     final_sequence = [None] * 10
     for ob in objects:
         char = ob.find('name').text
-
 
         Bounding_Box = ob.find('bndbox')
         xmin = int(Bounding_Box.find('xmin').text)
@@ -88,43 +105,28 @@ def read_from_xml(filepath, filename):
         ymin = int(Bounding_Box.find('ymin').text)
         ymax = int(Bounding_Box.find('ymax').text)
 
-        x_center = int(xmin + abs(xmax - xmin)/2)
-        y_centre = int(ymin + abs(ymax - ymin)/2)
+        x_center = int(xmin + abs(xmax - xmin) / 2)
+        y_centre = int(ymin + abs(ymax - ymin) / 2)
 
         Centre = [x_center, y_centre]
 
         Bounding_Box = [xmin, xmax, ymin, ymax]
         Char_data.append([char, Centre])
 
-        #print("* [{}], Charactor: '{}', Bounding_Box: [{}]".format(
-            #       filename, char, Bounding_Box
-            #    )
-            #   )
+        # print("* [{}], Charactor: '{}', Bounding_Box: [{}]".format(
+        #       filename, char, Bounding_Box
+        #    )
+        #   )
     random.shuffle(Char_data)
 
-    x = []
-    y = []
-    for i in range(len(Char_data)):
-        x.append(Char_data[i][1][0])
-        y.append(Char_data[i][1][1])
-    plt.plot(x, y)
-    ax = plt.gca()
-    ax.set_ylim([0, height])
-    ax.set_xlim([0, width])
-    plt.show()
-    #print(Char_data)
-    plot_diff(Char_data)
+    # print(Char_data)
+    shape_code = check_shape(Char_data, width, height)
     Arrange(filename, Char_data, final_sequence, width, height)
-
-
-
-
 
 
 for file in os.listdir("./Char-detection"):
     if file[-3:] == 'xml':
         read_from_xml("./Char-detection", file)
-
 
 '''  img = np.zeros((height, width, 3), dtype=np.uint8) 
     cv2.imshow("img", img)
